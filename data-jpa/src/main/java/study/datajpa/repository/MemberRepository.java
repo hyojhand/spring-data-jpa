b2538b40e287897ca117d4035d69662daa52abb1
hyojhand <sonjw5128@gmail.com>
2022-06-24 00:26:38 +0900

    [Add] Projections & Query By Example

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -14,7 +14,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, JpaSpecificationExecutor<Member> {
 
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
@@ -80,5 +80,8 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     // Lock
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String username);
+
+    // List<UsernameOnly> findProjectionsByUsername(@Param("username") String username);
+    List<UsernameOnlyDto> findProjectionsByUsername(@Param("username") String username);
 
 }
